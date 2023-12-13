bc614d28feff27c7279285a1c829172c7694633a
theluanhoang <hoangtheluan2016@gmail.com>
2023-12-13 15:59:09 +0700

    update function

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,35 @@ var router = express.Router();
 //Router này sẽ là webhook nhận thông tin giao dịch từ casso gọi qua được bảo mật bằng secure_token trong header
 router.route('/webhook/handler-bank-transfer')
     .post(async (req, res, next) => {
-        console.log("CALLING");
-        console.log("data: ", res.body);
-        res.status(200).json({ message: "Hello world" })
+        if (!req.header('secure-token') || req.header('secure-token') != secure_token) {
+            return res.status(401).json({
+                code: 401,
+                message: 'Missing secure-token or wrong secure-token'
+            })
+        }
+
+        console.log('req.data: ', req.body.data);
+        // B2: Thực hiện lấy thông tin giao dịch 
+        for (let item of req.body.data) {
+            // Lấy thông orderId từ nội dung giao dịch
+            let orderId = webhookUtil.parseOrderId(case_insensitive, transaction_prefix, item.description);
+            // Nếu không có orderId phù hợp từ nội dung ra next giao dịch tiếp theo
+            if (!orderId) continue;
+            // Kiểm tra giao dịch còn hạn hay không? Nếu không qua giao dịch tiếp theo
+            if ((((new Date()).getTime() - (new Date(item.when)).getTime()) / 86400000) >= expiration_date) continue;
+
+            console.log(orderId);
+            // Bước quan trọng đây.
+            // Sau khi có orderId Thì thực hiện thay đổi các trang thái giao dịch
+            // Ví dụ như kiểm tra orderId có tồn tại trong danh sách các đơn hàng của bạn?
+            // Sau đó cập nhật trạng thái theo orderId và amount nhận được: đủ hay thiếu tiền...
+            // Và một số chức năng khác có thể tùy biến
+        }
+        return res.status(200).json({
+            code: 200,
+            message: 'success',
+            data: null
+        })
     })
 // Router này sẽ thực hiện tính năng đồng bộ giao dịch tức thì.
 // Ví dụ: Khi người dùng chuyển khoản cho bạn và họ ấn nút tôi đã thanh toán thì nên xử lí gọi qua casso đề đồng bộ giao dịch vừa chuyển khoản
